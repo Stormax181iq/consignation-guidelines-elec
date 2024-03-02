516507f3c164409eaf5703779d7f5c125617564e
Stormax181iq <kelianchalier@gmail.com>
2024-03-02 10:28:54 +0100

    setting up the jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import SelectionPanel from "./components/SelectionPanel";
 export default function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">Hello World!</h1>
-      <p className="font-italic underline bg-slate-200">Test</p>
+      <SelectionPanel />
     </>
   );
 }
