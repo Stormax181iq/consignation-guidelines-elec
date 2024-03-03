2a88932c8f4bb691123568ed2ac235f655596b1e
Stormax181iq <kelianchalier@gmail.com>
2024-03-03 12:37:21 +0100

    added proper consignation types implementation and started cards for each step

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import { useState, useEffect } from "react";
 
 import SelectionPanel from "./components/SelectionPanel";
+import ConsignationGuide from "./components/ConsignationGuide";
 
 export default function App() {
-  const [data, setData] = useState(null);
+  const [consignationTypes, setConsignationTypes] = useState(null);
+  const [currentConsignation, setCurrentConsignation] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCurrentConsignation(e.target.children[0].value);
+  }
 
   useEffect(() => {
     let ignore = false;
     (async () => {
-      const data = await fetch("./data/consignationData.json").then(
+      const data = await fetch("./data/consignationTypes.json").then(
         (response) => response.json()
       );
 
       if (!ignore) {
-        setData(data);
+        setConsignationTypes(data);
       }
     })();
     return () => {
       ignore = true;
     };
   }, []);
-  return <SelectionPanel consignationTypes={data.consignationTypes} />;
+  return (
+    <>
+      <header>
+        {consignationTypes ? (
+          <SelectionPanel
+            consignationTypes={consignationTypes.consignationTypes}
+            onSubmit={handleSubmit}
+          />
+        ) : (
+          <p>Chargement ...</p>
+        )}
+      </header>
+      <main className="ml-2">
+        {currentConsignation !== "" && (
+          <ConsignationGuide consignation={currentConsignation} />
+        )}
+      </main>
+    </>
+  );
 }
