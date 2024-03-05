94665a544f7d2901ea38241c4c6c8a3a2856416f
Stormax181iq <kelianchalier@gmail.com>
2024-03-05 08:52:30 +0100

    add all src files to tailwind config

--- src/components/ConsignationGuide.jsx
+++ src/components/ConsignationGuide.jsx
@@ -38,7 +38,7 @@ function ConsignationCard({ step }) {
   const requiredElements = step.requiredElements;
 
   return (
-    <div>
+    <div className="border">
       <h1>{step.type.toUpperCase() + " - " + step.title}</h1>
       {todos && (
         <>
