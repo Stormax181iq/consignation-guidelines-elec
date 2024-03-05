9e685ad0974ba7185d4b17f8429ccadeeb22baad
Stormax181iq <kelianchalier@gmail.com>
2024-03-05 08:52:30 +0100

    add all src files to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.jsx"],
+  content: [
+    "./src/*.{jsx,js}",
+    "./src/components/*.{jsx,js}"
+],
   theme: {
     extend: {},
   },
