6cfc7d67c455629489bce4d90d3fe5b965646ef6
Stormax181iq <kelianchalier@gmail.com>
2024-03-09 20:02:39 +0100

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.jsx"],
+  content: ["./src/App.jsx", "./src/components/*.jsx"],
   theme: {
     extend: {},
   },
